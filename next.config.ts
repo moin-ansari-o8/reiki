0b9501acd607052f24d2d24d9503ba9dfffef830
Moin Ansari <moin.edu01@gmail.com>
2025-06-22 19:45:07 +0530

    optimized all codes again 6

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // 💥 THIS LINE FIXES YOUR DEPLOY ERRORS
-  },
-  // You can add more config options below if needed
+ 
 };
 
 export default nextConfig;
