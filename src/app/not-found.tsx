b28cca1c243d02c54e16bf3a2ff5c8731d2f5fe7
Moin Ansari <moin.edu01@gmail.com>
2025-06-22 19:53:39 +0530

    optimized all codes again 7

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,18 @@
 export default function NotFound() {
   return (
-    <html>
-      <body>
-        <h1>404 - Page Not Found</h1>
-        <p>The page you are looking for does not exist.</p>
-      </body>
-    </html>
+    <div
+      style={{
+        minHeight: "60vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1 style={{ fontSize: "2rem", color: "#5E3A87" }}>
+        404 - Page Not Found
+      </h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
   );
 }
